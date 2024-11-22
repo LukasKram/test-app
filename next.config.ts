e31b720e17d44caceac9c4458783f9d4db8c58ed
LukasKram <lukas.kramata@gmail.com>
2024-11-22 15:33:48 +0100

    disable redis

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
-  cacheHandler: process.env.NODE_ENV === "production" ? require.resolve("./cache-handler.js") : undefined,
+  output: "standalone",
+  //cacheHandler: process.env.NODE_ENV === "production" ? require.resolve("./cache-handler.js") : undefined,
 };
 
 export default nextConfig;
